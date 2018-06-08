a03ea9540b44d912d30d410d17f05ac7bc10b76c
Bhavisha <bhavishax@yahoo.co.uk>
2018-06-08 16:58:49 +0100

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,10 @@
+print ("hello world")
+lottery = [1,2,3,4,5]
+len(lottery)
+
+print('hello, Django girls!')
+1+1
+a=True
+a
+
+> cd %HomePath%\Djangogirls
---
+++ test.py
@@ -0,0 +1,9 @@
+print ("hello world")
+lottery = [1,2,3,4,5]
+len(lottery)
+
+print('hello, Django girls!')
+1+1
+a=True
+a
+
